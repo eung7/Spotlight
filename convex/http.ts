9d3502a4c9376d24546faca83210f4bf2c764a71
Eunzo <wannabeeung@gmail.com>
2025-06-18 22:20:03 +0900

    ✨ Sync clerk & database when user created

---
+++ convex/http.ts
@@ -0,0 +1,68 @@
+import { httpRouter } from "convex/server";
+import { Webhook } from "svix";
+import { api } from "./_generated/api";
+import { httpAction } from "./_generated/server";
+
+const http = httpRouter();
+
+http.route({
+  path: "/clerk-webhook",
+  method: "POST",
+  handler: httpAction(async (ctx, request) => {
+    const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+    if (!webhookSecret) {
+      throw new Error("CLERK_WEBHOOK_SECRET is not set");
+    }
+
+    const svix_id = request.headers.get("svix-id");
+    const svix_timestamp = request.headers.get("svix-timestamp");
+    const svix_signature = request.headers.get("svix-signature");
+
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+      return new Response("Missing headers", { status: 400 });
+    }
+
+    const payload = await request.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhookSecret);
+    let evt: any;
+
+    try {
+      evt = wh.verify(body, {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature,
+      });
+    } catch (error) {
+      console.error(error);
+      return new Response("Invalid request", { status: 400 });
+    }
+
+    const eventType = evt.type;
+
+    if (eventType === "user.created") {
+      const { id, email_addresses, first_name, last_name, image_url } =
+        evt.data;
+
+      const email = email_addresses[0].email_address;
+      const name = `${first_name} ${last_name}`;
+
+      try {
+        await ctx.runMutation(api.users.createUser, {
+          clerkId: id,
+          fullname: name,
+          email,
+          username: email.split("@")[0],
+          image: image_url,
+        });
+      } catch {
+        console.error("Failed to create user");
+        return new Response("Failed to create user", { status: 500 });
+      }
+    }
+    return new Response("User created", { status: 200 });
+  }),
+});
+
+export default http;
