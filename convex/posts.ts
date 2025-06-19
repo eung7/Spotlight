544ffb07e6565eec73b0f083bfc277395c0ca4d7
Eunzo <wannabeeung@gmail.com>
2025-06-19 22:17:44 +0900

    ✨ Create Post Screen Implemention

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,6 +1,12 @@
 import { v } from "convex/values";
 import { mutation } from "./_generated/server";
 
+export const generateUploadUrl = mutation(async (ctx) => {
+  const identity = await ctx.auth.getUserIdentity();
+  if (!identity) throw new Error("Unauthorized");
+  return await ctx.storage.generateUploadUrl();
+});
+
 export const createPost = mutation({
   args: {
     caption: v.optional(v.string()),
